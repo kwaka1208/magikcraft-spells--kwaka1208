d9b530b2aef2821941ff5b827093f0c168089a04
kwaka1208 <kwaka1208@pote2.net>
2017-11-23 07:18:16 +0000

    Update remember.ts

--- remember.ts
+++ remember.ts
@@ -1,6 +1,6 @@
 const magik = magikcraft.io;
 
-function remember(name = "default") {
+function remember(name = "default.place") {
     const here = magik.hic();
     magik.memento.setItem(name, here);
 }

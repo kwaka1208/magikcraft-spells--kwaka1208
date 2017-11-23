ba552e4f2d545fe6573daa057642436ab0f3c4cc
kwaka1208 <kwaka1208@pote2.net>
2017-11-23 07:27:06 +0000

    Update recall.ts

--- recall.ts
+++ recall.ts
@@ -3,5 +3,9 @@ const magik = magikcraft.io;
 function recall(name = "default.place"){
     const there = magik.memento.getItem(name);
     magik.dixit("Recalling:" + name);
-    magik.dixit(there.toString());
+    if(there) {
+        magik.dixit(there.toString());
+    } else {
+        magik.dixit("No memory to recall");
+    }
 }
